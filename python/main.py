4b993f14997f144c787a19c074f76ae768b1c1ec
azzyjk <56161518+azzyjk@users.noreply.github.com>
2020-06-06 17:00:35 +0900

    Crawling COVID data from Disease control center

---
+++ python/main.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import requests
+from function import *
+import time
+
+site = "http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=12&ncvContSeq=&contSeq=&board_id=&gubun="
+
+page = requests.get(site)
+bsObject = BeautifulSoup(page.text, "html.parser")
+infoCOVID = bsObject.find_all('td')
+strCOVID = replaceTag(str(infoCOVID))
+
+strLists = strCOVID.split('\n')
+COIVDlists = []
+
+for data in strLists:
+    checkLoc(COIVDlists, str(data))
+
+print(COIVDlists)
