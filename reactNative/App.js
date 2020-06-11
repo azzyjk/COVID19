7ceb116b926cafa87f6ce34ca4b0ad32577e94eb
azzyjk <56161518+azzyjk@users.noreply.github.com>
2020-06-11 21:43:40 +0900

    Made app that show many marker about 'COVID 19'

--- reactNative/App.js
+++ reactNative/App.js
@@ -3,13 +3,15 @@ import { StyleSheet, Text, View, Alert, Dimensions } from "react-native";
 import axios from "axios";
 import { TMAP_API } from "react-native-dotenv";
 import * as Location from "expo-location";
-import MapView, { Marker } from "react-native-maps";
+import MapView, { Marker, Circle } from "react-native-maps";
+import Map from "./Map";
+import uuidv1 from "uuid/v1";
 
 export default class App extends React.Component {
   state = {
-    usrLat: "",
-    usrLon: "",
-    COVID: [],
+    usrLat: 0,
+    usrLon: 0,
+    COVID: {},
   };
   componentDidMount() {
     this._getUserLocation();
@@ -22,22 +24,49 @@ export default class App extends React.Component {
         <MapView
           style={styles.mapStyle}
           initialRegion={{
-            latitude: 37.412068,
-            longitude: 126.974086,
-            latitudeDelta: 0.0002,
-            longitudeDelta: 0.0021,
+            latitude: 37.410717,
+            longitude: 126.972513,
+            latitudeDelta: 0.0052,
+            longitudeDelta: 0.0061,
           }}
         >
-          <Marker
-            coordinate={{
-              latitude: 37.412068,
-              longitude: 126.974086,
+          <Circle
+            center={{
+              latitude: usrLat,
+              longitude: usrLon,
             }}
+            radius={50}
+            strokeWidth={2}
+            strokeColor="#000DFF"
           />
+          {Object.values(COVID).map((location) => (
+            <Map key={location.id} {...location} />
+          ))}
         </MapView>
       </View>
     );
   }
+  _addCOVID = (address, longitude, latitude) => {
+    this.setState((prevState) => {
+      const ID = uuidv1();
+      const newCOVIDObject = {
+        [ID]: {
+          id: ID,
+          address: address,
+          latitude: parseFloat(latitude),
+          longitude: parseFloat(longitude),
+        },
+      };
+      const newState = {
+        ...prevState,
+        COVID: {
+          ...prevState.COVID,
+          ...newCOVIDObject,
+        },
+      };
+      return { ...newState };
+    });
+  };
   _noticeAlert() {
     Alert.alert("위험", "코로나 위험지역과 500m내에 있습니다.");
   }
@@ -77,6 +106,7 @@ export default class App extends React.Component {
          &appKey=${TMAP_API}`
       );
       this._getDistance(coordinate[0].newLon, coordinate[0].newLat);
+      this._addCOVID(address, coordinate[0].newLon, coordinate[0].newLat);
     } catch (error) {
       console.log(error);
     }
@@ -89,9 +119,6 @@ export default class App extends React.Component {
       loc.forEach((element) => {
         this._getCOIVDLonLat(element);
       });
-      this.setState({
-        COVID: loc,
-      });
     } catch (error) {
       console.log(error);
     }
@@ -101,6 +128,7 @@ export default class App extends React.Component {
     const {
       coords: { latitude, longitude },
     } = await Location.getCurrentPositionAsync();
+    this._addCOVID("UserLocation", longitude, latitude);
     this.setState({
       usrLat: latitude,
       usrLon: longitude,
---
+++ reactNative/Map.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Alert, Dimensions } from "react-native";
+import MapView, { Marker, Circle } from "react-native-maps";
+import PropTypes from "prop-types";
+
+export default class Map extends React.Component {
+  static propTypes = {
+    longitude: PropTypes.number.isRequired,
+    latitude: PropTypes.number.isRequired,
+  };
+  render() {
+    const { id, address, latitude, longitude } = this.props;
+    return (
+      <Marker
+        coordinate={{
+          latitude: latitude,
+          longitude: longitude,
+        }}
+        title={address}
+        description="test"
+      ></Marker>
+    );
+  }
+}
